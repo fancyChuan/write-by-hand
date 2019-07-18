9ce7cb12ee8ad331910285d19420b96b21c35808
fancyChuan <1247375074@qq.com>
2019-07-18 23:59:25 +0800

    rpc：socket Echo demo代码修改

--- rpc/src/cn/fancychuan/prepare/socket/EchoSocketClient.java
+++ rpc/src/cn/fancychuan/prepare/socket/EchoSocketClient.java
@@ -10,8 +10,8 @@ import java.util.Scanner;
 public class EchoSocketClient {
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("localhost", 1000);
-        PrintStream serverPs = new PrintStream(socket.getOutputStream());
-        Scanner inputScanner = new Scanner(new InputStreamReader(System.in));
+        PrintStream serverPs = new PrintStream(socket.getOutputStream());  // 从服务端接收到的消息
+        Scanner inputScanner = new Scanner(new InputStreamReader(System.in)); // 扫描用户键盘输入
         inputScanner.useDelimiter("\n");
         boolean flag = true;
         while (flag) {
@@ -21,12 +21,16 @@ public class EchoSocketClient {
                 serverPs.println(content);
                 serverPs.flush();
                 BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                String fromServer = br.readLine();
+                String fromServer = br.readLine(); // BufferedReader是一个缓冲流，会根据"\n"字符切分为多行，这里默认就取第一行
                 System.out.println("[server]" + fromServer);
                 if (fromServer.contains("bye bye")) {
                     flag = false;
                 }
             }
         }
+        serverPs.close();
+        inputScanner.close();
+        socket.close();
+        System.out.println("closed");
     }
 }
--- rpc/src/cn/fancychuan/prepare/socket/EchoSocketServer.java
+++ rpc/src/cn/fancychuan/prepare/socket/EchoSocketServer.java
@@ -11,35 +11,33 @@ import java.util.Scanner;
 public class EchoSocketServer {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(1000);
-        while (true) {
-            // 接收到客户端的socket请求，服务端也对应产生一个socket
-            Socket accept = serverSocket.accept();
-            InputStream clientInput = accept.getInputStream(); // 获取客户端输入的内容
-            OutputStream clientOutput = accept.getOutputStream(); // 往客户端写回消息
-
-            // 使用PrintStream方便往客户端写内容
-            PrintWriter ps = new PrintWriter(clientOutput);
-            // 处理输入数据
-            Scanner scanner = new Scanner(clientInput);
-            scanner.useDelimiter("\n");
-            boolean flag = true;
-            while (flag) {
-                if (scanner.hasNext()) {
-                    String content = scanner.next();
-                    System.out.println("[from client]" + content);
-                    if ("exit".equalsIgnoreCase(content)) {
-                        ps.println("[server] bye bye ~");
-                        flag = false;
-                    } else {
-                        System.out.println("[client] " + content);
-                        ps.println("[server] received message: " + content); // 通知客户端已收到消息
-                        ps.flush();
-                    }
+        // 接收到客户端的socket请求，服务端也对应产生一个socket。这里是单线程的，也就是接受了服务之后就无法接受其他客户端的连接了
+        Socket clientSocket = serverSocket.accept();
+        // 处理输入数据
+        Scanner scanner = new Scanner(clientSocket.getInputStream()); // 获取客户端输入的内容
+        scanner.useDelimiter("\n");
+        // 使用PrintStream是通过系统标准输入输出流和客户端通信
+        PrintWriter ps = new PrintWriter(clientSocket.getOutputStream()); // 往客户端写回消息
+        boolean flag = true; //循环标识
+        while (flag) {
+            if (scanner.hasNext()) {
+                String content = scanner.next();
+                System.out.println("[from client]" + content);
+                if ("exit".equalsIgnoreCase(content)) {
+                    ps.println("[server] bye bye ~");
+                    flag = false;
+                } else {
+                    System.out.println("[client] " + content);
+                    ps.println("received message: " + content); // 通知客户端已收到消息
+                    ps.flush(); // 刷新输出流，立刻写回客户端
                 }
             }
-
-            accept.close();
-            serverSocket.close();
         }
+
+        scanner.close();
+        ps.close();
+        clientSocket.close();
+        serverSocket.close();
+        System.out.println("closed");
     }
 }
